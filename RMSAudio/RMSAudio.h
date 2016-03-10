75f332da7637c2b962e8ca98ee58d4ca401f32b9
32Beat <opgr@32bts-macbook-pro.local>
2016-03-10 23:09:07 +0100

    Initial Commit

---
+++ RMSAudio/RMSAudio.h
@@ -0,0 +1,19 @@
+//
+//  RMSAudio.h
+//  RMSAudio
+//
+//  Created by 32BT on 10/03/16.
+//  Copyright © 2016 32BT. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RMSAudio.
+FOUNDATION_EXPORT double RMSAudioVersionNumber;
+
+//! Project version string for RMSAudio.
+FOUNDATION_EXPORT const unsigned char RMSAudioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMSAudio/PublicHeader.h>
+
+
